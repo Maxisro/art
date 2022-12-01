8c73816772871296864759339431ceea4bda1853
Maksym Pecherytsia <findimrk@gmail.com>
2022-12-01 22:01:55 +0200

    added function for uploading cards through using GET response from db

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,6 @@
 // import checkNumInputs from "./checkNuminputs";
+import { postData } from "../services/requests";
+
 
 const forms = () => {
 	const form = document.querySelectorAll("form"),
@@ -21,14 +23,7 @@ const forms = () => {
 		question: "assets/question.php",
 	};
 
-	const postData = async (url, data) => {
-		let res = await fetch(url, {
-			method: "POST",
-			body: data,
-		});
-
-		return await res.text();
-	};
+	
 
 	const clearInputs = () => {
 		inputs.forEach((item) => {
